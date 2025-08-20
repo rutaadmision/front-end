9ff0ae2767d2c72e53267b70b477dc588470ae60
ronny <ronny@localhost.localdomain>
2025-08-19 19:48:11 -0600

    formBuilder

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,34 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { Router, RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, Validators, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule,RouterLink],
+  imports: [RouterLink, ReactiveFormsModule,JsonPipe],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  username = '';
-  password = '';
 
-  constructor(private auth: AuthService, private router: Router) {}
+  private fb = inject(FormBuilder);
+
+  loginForm = this.fb.group({
+    'email': ['', [Validators.required, Validators.email]],
+    'password': ['', [Validators.required]]
+  });
+
+  get email() {
+    return this.loginForm.get('email') as FormControl;
+  }
+
+  get password() {
+    return this.loginForm.get('password') as FormControl;
+  }
+
+  constructor(private auth: AuthService, private router: Router) { }
+
 
   // Login method
   onLogin() {
-    this.auth.login(this.username, this.password).subscribe({
+    console.log(this.loginForm.valid);
+    /*this.auth.login(this.username, this.password).subscribe({
       next: () => this.router.navigate(['/problems']),
       error: (err) => alert('Login failed: ' + err.message),
-    });
+    });*/
   }
 
   // Signup method
-  onSignup() {
+  /*onSignup() {
     this.auth.signup(this.username, this.password).subscribe({
       next: () => this.router.navigate(['/problems']),
       error: (err) => alert('Signup failed: ' + err.message),
     });
-  }
+  }*/
 }
