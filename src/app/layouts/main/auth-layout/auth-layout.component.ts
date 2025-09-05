b59bda0b59d741818e62b11aab977b9c9cd5b55f
Ronny <ronnyale0@hotmail.com>
2025-09-05 01:02:40 +0000

    Boton en la esquina

--- src/app/layouts/main/auth-layout/auth-layout.component.ts
+++ src/app/layouts/main/auth-layout/auth-layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { MapRoutes } from '../../../map-routes';
 @Component({
   selector: 'app-auth-layout',
   imports: [RouterModule],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.css',
 })
-export class AuthLayoutComponent {}
+export class AuthLayoutComponent {
+  MapRoutes = MapRoutes;
+}
