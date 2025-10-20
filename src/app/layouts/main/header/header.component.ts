745356134abc30b8396e71b87a6feb553784c7a1
Ronny <ronnyale0@hotmail.com>
2025-10-20 17:23:07 +0000

    Done

--- src/app/layouts/main/header/header.component.ts
+++ src/app/layouts/main/header/header.component.ts
@@ -1,6 +1,11 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { LogOutIcon, LucideAngularModule, TargetIcon } from 'lucide-angular';
+import {
+  LogOutIcon,
+  LucideAngularModule,
+  TargetIcon,
+  UserIcon,
+} from 'lucide-angular';
 import { AuthService } from '../../../core/services/auth.service';
 import { MapRoutes } from '../../../map-routes';
 
@@ -14,14 +19,11 @@ export class HeaderComponent implements OnInit {
   MapRoutes = MapRoutes;
   readonly TargetIcon = TargetIcon;
   readonly LogOutIcon = LogOutIcon;
+  readonly UserIcon = UserIcon;
   private authService = inject(AuthService);
   isLogin = signal(false);
   ngOnInit(): void {
     this.isLogin.set(this.authService.isLoggedIn());
-  }
-
-  test() {
-    alert('as');
   }
 
   logout() {
