6dd7d2b67dbbe70a2ff2f11f2abcb2c0cf639e32
Ronny <ronnyale0@hotmail.com>
2025-10-25 18:12:24 +0000

    Inicio

--- src/app/features/problems/problems.component.ts
+++ src/app/features/problems/problems.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
+import {
+  ChevronLeftIcon,
+  ClockIcon,
+  LucideAngularModule,
+} from 'lucide-angular';
 import { AuthService } from '../../core/services/auth.service';
-
 @Component({
   selector: 'app-problems',
   standalone: true,
+  imports: [LucideAngularModule],
   templateUrl: './problems.component.html',
   styleUrls: ['./problems.component.css'],
 })
 export class ProblemsComponent {
+  readonly ChevronLeftIcon = ChevronLeftIcon;
+  readonly ClockIcon = ClockIcon;
   constructor(private auth: AuthService) {}
   logout() {
     this.auth.logout();
